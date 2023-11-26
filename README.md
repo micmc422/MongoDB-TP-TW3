2fda029a8116a5def5aa3e10a0ee0736b1dad8b9
micmc422 <micmc422@gmail.com>
2023-11-26 23:20:45 +0000

    feat: Update devcontainer.json and package.json
    Updated devcontainer.json and package.json to use Node.js image and MongoDB package

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 étapes :
 - Utiliser le cours pour installer mongoDB dans le codesandBox.
 - Suivre les consignes d'initialisation de l'exercice.
-- S'approprié le code est ajouter des fonctionalités, des actions pour interagir avec mongoo.
+- S'approprié le code et ajouter des fonctionalitées, des actions pour interagir avec mongoo.
--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://0.0.0.0:27017', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+const mongoUrl = "mongodb://0.0.0.0:27017";
 
 app.use(bodyParser.json());
 
-app.use(express.static('src/public'));
+app.use(express.static("src/public"));
 
-app.post('/sendData', (req, res) => {
-  const data = req.body.data;
+app.post("/addData", async (req, res) => {
+  const dataContent = req.body.data;
 
-  // Faites quelque chose avec les données, par exemple, enregistrez-les dans MongoDB
-  // Pour cet exemple, nous renvoyons simplement un message
-  res.json({ message: `Data received: ${data}` });
+  try {
+    // Établir une connexion avec la base de données
+    const client = new MongoClient(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+
+    // Sélectionner la collection
+    const collection = client.db().collection("data");
+
+    // Insérer une nouvelle donnée dans la collection
+    await collection.insertOne({ content: dataContent });
+
+    // Fermer la connexion
+    await client.close();
+
+    res.json({ message: "Data added successfully" });
+  } catch (error) {
+    console.error("Error adding data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, MongoDB!');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+// ... (autres routes)
